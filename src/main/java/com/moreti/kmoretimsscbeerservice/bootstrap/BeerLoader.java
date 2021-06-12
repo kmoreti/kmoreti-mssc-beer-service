afa4351a88b13415139b63ee39d88d3ca140d1ed
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2021-06-12 20:13:48 +0100

    Added MySQL

--- src/main/java/com/moreti/kmoretimsscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/moreti/kmoretimsscbeerservice/bootstrap/BeerLoader.java
@@ -2,11 +2,13 @@ package com.moreti.kmoretimsscbeerservice.bootstrap;
 
 import com.moreti.kmoretimsscbeerservice.domain.Beer;
 import com.moreti.kmoretimsscbeerservice.repositories.BeerRepository;
+import com.moreti.kmoretimsscbeerservice.web.model.BeerStyleEnum;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
-//@Component
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
@@ -21,38 +23,41 @@ public class BeerLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        loadBeerObjects();
+        if (beerRepository.count() == 0) {
+            loadBeerObjects();
+        }
     }
 
     private void loadBeerObjects() {
+        Beer beer1 = Beer.builder()
+                .beerName("Mango Bobs")
+                .beerStyle(BeerStyleEnum.IPA.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .upc(BEER_1_UPC)
+                .price(new BigDecimal("12.95"))
+                .build();
 
-        if (beerRepository.count() == 0) {
-            beerRepository.save(Beer.builder()
-                    .beerName("Mango Bobs")
-                    .beerStyle("IPA")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_1_UPC)
-                    .price(new BigDecimal("12.95"))
-                    .build());
+        Beer beer2 = Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .upc(BEER_2_UPC)
+                .price(new BigDecimal("12.95"))
+                .build();
 
-            beerRepository.save(Beer.builder()
-                    .beerName("Galaxy Cat")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_2_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .build());
+        Beer beer3 = Beer.builder()
+                .beerName("Pinball Porter")
+                .beerStyle(BeerStyleEnum.PORTER.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .upc(BEER_3_UPC)
+                .price(new BigDecimal("12.95"))
+                .build();
 
-            beerRepository.save(Beer.builder()
-                    .beerName("No Hammers On The Bar")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_3_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .build());
-        }
+        beerRepository.save(beer1);
+        beerRepository.save(beer2);
+        beerRepository.save(beer3);
     }
 }
